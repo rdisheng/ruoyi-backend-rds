23e4a463af7e1612118c0227c391884aae201842
rds <2432363698@qq.com>
2025-08-24 18:34:14 +0800

    解决测试类依赖版本冲突问题

--- ruoyi-admin/src/test/java/com/ruoyi/ai/AiTest.java
+++ ruoyi-admin/src/test/java/com/ruoyi/ai/AiTest.java
@@ -2,11 +2,14 @@ package com.ruoyi.ai;
 
 import com.ruoyi.common.core.redis.RedisCache;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Map;
+import java.util.Set;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class AiTest {
 
@@ -16,9 +19,9 @@ public class AiTest {
     @Test
     public void testChatAi() {
         System.out.println("集成测试，testChatAi");
-        Map<String, Object> map = redisCache.getAllCache();
-        map.forEach((key, value) -> {
-            System.out.println("key = " + key + ", value = " + value);
+        Set<String> keys = redisCache.getAllCache();
+        keys.forEach(e -> {
+            System.out.println(e);
         });
     }
 }
