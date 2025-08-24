105be73730f747d6adf2c68786e5f21d8e9db611
rds <2432363698@qq.com>
2025-08-24 17:22:06 +0800

    添加测试类

---
+++ ruoyi-admin/src/test/java/com/ruoyi/ai/AiTest.java
@@ -0,0 +1,16 @@
+package com.ruoyi.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class AiTest {
+
+    /*@Autowired
+    private xxxServiceImpl xxxService;*/
+
+    @Test
+    void testChatAi() {
+        System.out.println("集成测试，testChatAi");
+    }
+}
---
+++ ruoyi-ai/src/test/java/com/ruoyi/ai/AiTest.java
@@ -0,0 +1,11 @@
+package com.ruoyi.ai;
+
+import org.junit.jupiter.api.Test;
+
+class AiTest {
+
+    @Test
+    void testAsk() {
+        System.out.println("hello");
+    }
+}
